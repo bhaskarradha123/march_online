cbcbdf51524e78e9bcb14466272176671ccd1896
Excelr Ed tech <mailmeasradha@gmail.com>
2026-04-10 19:20:53 +0530

    functions and number programs

--- js/functions.js
+++ js/functions.js
@@ -1,13 +1,45 @@
 // function declaration
+// f1()//hoisting 
+// function f1(){  // no arg functions
+//     console.log('hello world');  
+// }
 
-function f1(){
-    console.log('hello world');
+// f1(); //calling a function --function caller
+// f1();
+// f1();
+// function f2(name){ // arg function
+//     console.log(`my name is ${name}`);
+    
+// }
+// f2(); //my name is undefined
+// f2('radha');//my name is radha
+// f2('bhaskar');//my name is bhaskar
+
+
+
+
+// function expression
+let f1=function(){
+    console.log("function expression");
     
 }
+f1();
 
-f1(); //calling a function --function caller
 
 
+// arrow function or shorter syntax for function
+let f3=()=>{
+  console.log("arrow function");
+  
+}
+f3();
+
+//Anonymous Function
+setTimeout(f1(), 5000);
+
+setTimeout(() => {
+    console.log("anonymous function");
+}, timeout);
 
 
 
