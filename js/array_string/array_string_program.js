c5117307253bd8d51e26d90967d73fdb61dec09d
Excelr Ed tech <mailmeasradha@gmail.com>
2026-04-20 19:56:56 +0530

    array and dom

--- js/array_string/array_string_program.js
+++ js/array_string/array_string_program.js
@@ -14,7 +14,7 @@ let arr=[107,3,432,23,44];
 let  sum=arr.reduce(
     (sum,value)=>{
         return sum+value;
-    }
+    },0
 );
 console.log(sum);
 
@@ -46,7 +46,47 @@ console.log(sum);
 //    higher to lower marks
     let sortedUsers= users.sort((a,b)=>b.marks-a.marks);
      console.log(sortedUsers);
-     
+
+     sortedUsers.reverse();
+
+// =================Remove duplicates in an array==========
+let arr2=["apple",'ball','apple','carrot','dog','dog']
+    let unique= new Set(arr2);
+    console.log(arr2);
+    console.log(unique);
+
+// ===============Find second largest number in an array==============
+    let arr3=[10,34,12,34,44,44];
+    let uniqureArray=[...new Set(arr3)]; // converting array into set and storing set in array format
+    uniqureArray.sort((a,b)=>b-a); // sorted in descending order    
+    console.log('second maximum',uniqureArray[1]);  //printing secong largest number based on index
+    console.log('second minimum',uniqureArray[uniqureArray.length-2]);  //printing secong smallest number based on index
+    
+
+    // or
+
+    let flargest=-Infinity;
+    let slargest=-Infinity;
+    for (let index = 0; index < arr3.length; index++) {
+        const element = arr3[index];
+        if(element>flargest){
+            slargest=flargest;
+            flargest=element;
+        }
+        else if(element>slargest&&element!==flargest){    //10 7 8 9
+             slargest= element;
+        }
+        
+    }
+    console.log(flargest);
+    console.log(slargest);
+    
+    
+    
+    
+   
+    
+
     
  
 
